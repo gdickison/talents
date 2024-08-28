25dbd8cca3bc3fc6ea91eea14509bcfa4e99f8a5
paunchymonk <gregory@dickison.net>
2024-08-28 09:40:27 -0700

    Finish first design of individual product view

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -2,47 +2,21 @@
 async function Product4 ({ params }) {
   const data = await fetch(`https://dummyjson.com/products/${params.id}`)
   const product = await data.json()
-  console.log('product', product)
 
   return (
-    <div className='flex flex-col items-center mt-12 relative w-[375px] mx-auto text-white bg-t-brown'>
-      <div className="text-white text-[23px] font-normal font-['Marion']">
-        <div className="2xl:container 2xl:mx-auto py-2 px-3">
+    <div className='flex flex-col items-center mt-12 relative w-[375px] mx-auto text-white bg-t-brown font-dm-sans'>
+      <div className="text-white font-normal">
+        <div className="py-2 px-3">
           <div className="flex justify-center items-center flex-col gap-8">
-            <div className="  w-full items-center">
-              <h2 className="font-marion lg:text-4xl text-3xl lg:leading-9 leading-7 mt-4">{product.title}</h2>
+            <div className="w-full items-center">
+              <h2 className="font-dm-sans text-3xl leading-9 mt-4">{product.title}</h2>
               <div className=" flex flex-row justify-between  mt-5">
                 <div className=" flex flex-row space-x-3">
-                  <svg className=" cursor-pointer" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M15.5598 20C15.3998 20.0006 15.2421 19.9629 15.0998 19.89L9.99976 17.22L4.89976 19.89C4.73416 19.977 4.54744 20.0159 4.36084 20.0022C4.17424 19.9884 3.99524 19.9226 3.84419 19.8122C3.69314 19.7017 3.5761 19.5511 3.50638 19.3775C3.43665 19.2039 3.41704 19.0142 3.44976 18.83L4.44976 13.2L0.329763 9.19996C0.20122 9.07168 0.110034 8.91083 0.0659903 8.73465C0.0219465 8.55848 0.0267076 8.37363 0.0797626 8.19996C0.137723 8.02223 0.244339 7.86431 0.387513 7.74412C0.530687 7.62392 0.704685 7.54627 0.889763 7.51996L6.58976 6.68996L9.09976 1.55996C9.18165 1.39089 9.3095 1.2483 9.46867 1.14853C9.62785 1.04876 9.81191 0.99585 9.99976 0.99585C10.1876 0.99585 10.3717 1.04876 10.5309 1.14853C10.69 1.2483 10.8179 1.39089 10.8998 1.55996L13.4398 6.67996L19.1398 7.50996C19.3248 7.53627 19.4988 7.61392 19.642 7.73412C19.7852 7.85431 19.8918 8.01223 19.9498 8.18996C20.0028 8.36363 20.0076 8.54848 19.9635 8.72465C19.9195 8.90083 19.8283 9.06168 19.6998 9.18996L15.5798 13.19L16.5798 18.82C16.6155 19.0074 16.5968 19.2012 16.5259 19.3784C16.455 19.5556 16.3349 19.7088 16.1798 19.82C15.9987 19.9469 15.7806 20.0102 15.5598 20Z"
-                      fill="#ECD592"
-                    />
-                  </svg>
-                  <svg className=" cursor-pointer" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M15.5598 20C15.3998 20.0006 15.2421 19.9629 15.0998 19.89L9.99976 17.22L4.89976 19.89C4.73416 19.977 4.54744 20.0159 4.36084 20.0022C4.17424 19.9884 3.99524 19.9226 3.84419 19.8122C3.69314 19.7017 3.5761 19.5511 3.50638 19.3775C3.43665 19.2039 3.41704 19.0142 3.44976 18.83L4.44976 13.2L0.329763 9.19996C0.20122 9.07168 0.110034 8.91083 0.0659903 8.73465C0.0219465 8.55848 0.0267076 8.37363 0.0797626 8.19996C0.137723 8.02223 0.244339 7.86431 0.387513 7.74412C0.530687 7.62392 0.704685 7.54627 0.889763 7.51996L6.58976 6.68996L9.09976 1.55996C9.18165 1.39089 9.3095 1.2483 9.46867 1.14853C9.62785 1.04876 9.81191 0.99585 9.99976 0.99585C10.1876 0.99585 10.3717 1.04876 10.5309 1.14853C10.69 1.2483 10.8179 1.39089 10.8998 1.55996L13.4398 6.67996L19.1398 7.50996C19.3248 7.53627 19.4988 7.61392 19.642 7.73412C19.7852 7.85431 19.8918 8.01223 19.9498 8.18996C20.0028 8.36363 20.0076 8.54848 19.9635 8.72465C19.9195 8.90083 19.8283 9.06168 19.6998 9.18996L15.5798 13.19L16.5798 18.82C16.6155 19.0074 16.5968 19.2012 16.5259 19.3784C16.455 19.5556 16.3349 19.7088 16.1798 19.82C15.9987 19.9469 15.7806 20.0102 15.5598 20Z"
-                      fill="#ECD592"
-                    />
-                  </svg>
-                  <svg className=" cursor-pointer" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M15.5598 20C15.3998 20.0006 15.2421 19.9629 15.0998 19.89L9.99976 17.22L4.89976 19.89C4.73416 19.977 4.54744 20.0159 4.36084 20.0022C4.17424 19.9884 3.99524 19.9226 3.84419 19.8122C3.69314 19.7017 3.5761 19.5511 3.50638 19.3775C3.43665 19.2039 3.41704 19.0142 3.44976 18.83L4.44976 13.2L0.329763 9.19996C0.20122 9.07168 0.110034 8.91083 0.0659903 8.73465C0.0219465 8.55848 0.0267076 8.37363 0.0797626 8.19996C0.137723 8.02223 0.244339 7.86431 0.387513 7.74412C0.530687 7.62392 0.704685 7.54627 0.889763 7.51996L6.58976 6.68996L9.09976 1.55996C9.18165 1.39089 9.3095 1.2483 9.46867 1.14853C9.62785 1.04876 9.81191 0.99585 9.99976 0.99585C10.1876 0.99585 10.3717 1.04876 10.5309 1.14853C10.69 1.2483 10.8179 1.39089 10.8998 1.55996L13.4398 6.67996L19.1398 7.50996C19.3248 7.53627 19.4988 7.61392 19.642 7.73412C19.7852 7.85431 19.8918 8.01223 19.9498 8.18996C20.0028 8.36363 20.0076 8.54848 19.9635 8.72465C19.9195 8.90083 19.8283 9.06168 19.6998 9.18996L15.5798 13.19L16.5798 18.82C16.6155 19.0074 16.5968 19.2012 16.5259 19.3784C16.455 19.5556 16.3349 19.7088 16.1798 19.82C15.9987 19.9469 15.7806 20.0102 15.5598 20Z"
-                      fill="#ECD592"
-                    />
-                  </svg>
-                  <svg className=" cursor-pointer" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M15.5598 20C15.3998 20.0006 15.2421 19.9629 15.0998 19.89L9.99976 17.22L4.89976 19.89C4.73416 19.977 4.54744 20.0159 4.36084 20.0022C4.17424 19.9884 3.99524 19.9226 3.84419 19.8122C3.69314 19.7017 3.5761 19.5511 3.50638 19.3775C3.43665 19.2039 3.41704 19.0142 3.44976 18.83L4.44976 13.2L0.329763 9.19996C0.20122 9.07168 0.110034 8.91083 0.0659903 8.73465C0.0219465 8.55848 0.0267076 8.37363 0.0797626 8.19996C0.137723 8.02223 0.244339 7.86431 0.387513 7.74412C0.530687 7.62392 0.704685 7.54627 0.889763 7.51996L6.58976 6.68996L9.09976 1.55996C9.18165 1.39089 9.3095 1.2483 9.46867 1.14853C9.62785 1.04876 9.81191 0.99585 9.99976 0.99585C10.1876 0.99585 10.3717 1.04876 10.5309 1.14853C10.69 1.2483 10.8179 1.39089 10.8998 1.55996L13.4398 6.67996L19.1398 7.50996C19.3248 7.53627 19.4988 7.61392 19.642 7.73412C19.7852 7.85431 19.8918 8.01223 19.9498 8.18996C20.0028 8.36363 20.0076 8.54848 19.9635 8.72465C19.9195 8.90083 19.8283 9.06168 19.6998 9.18996L15.5798 13.19L16.5798 18.82C16.6155 19.0074 16.5968 19.2012 16.5259 19.3784C16.455 19.5556 16.3349 19.7088 16.1798 19.82C15.9987 19.9469 15.7806 20.0102 15.5598 20Z"
-                      fill="#ECD592"
-                    />
-                  </svg>
-                  <svg className=" cursor-pointer" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M15.5598 20C15.3998 20.0006 15.2421 19.9629 15.0998 19.89L9.99976 17.22L4.89976 19.89C4.73416 19.977 4.54744 20.0159 4.36084 20.0022C4.17424 19.9884 3.99524 19.9226 3.84419 19.8122C3.69314 19.7017 3.5761 19.5511 3.50638 19.3775C3.43665 19.2039 3.41704 19.0142 3.44976 18.83L4.44976 13.2L0.329763 9.19996C0.20122 9.07168 0.110034 8.91083 0.0659903 8.73465C0.0219465 8.55848 0.0267076 8.37363 0.0797626 8.19996C0.137723 8.02223 0.244339 7.86431 0.387513 7.74412C0.530687 7.62392 0.704685 7.54627 0.889763 7.51996L6.58976 6.68996L9.09976 1.55996C9.18165 1.39089 9.3095 1.2483 9.46867 1.14853C9.62785 1.04876 9.81191 0.99585 9.99976 0.99585C10.1876 0.99585 10.3717 1.04876 10.5309 1.14853C10.69 1.2483 10.8179 1.39089 10.8998 1.55996L13.4398 6.67996L19.1398 7.50996C19.3248 7.53627 19.4988 7.61392 19.642 7.73412C19.7852 7.85431 19.8918 8.01223 19.9498 8.18996C20.0028 8.36363 20.0076 8.54848 19.9635 8.72465C19.9195 8.90083 19.8283 9.06168 19.6998 9.18996L15.5798 13.19L16.5798 18.82C16.6155 19.0074 16.5968 19.2012 16.5259 19.3784C16.455 19.5556 16.3349 19.7088 16.1798 19.82C15.9987 19.9469 15.7806 20.0102 15.5598 20ZM9.99976 15.1C10.1601 15.0959 10.3186 15.1338 10.4598 15.21L14.2298 17.21L13.5098 13C13.4818 12.8392 13.4936 12.6741 13.5442 12.5189C13.5947 12.3638 13.6825 12.2234 13.7998 12.11L16.7998 9.17996L12.5998 8.55996C12.4457 8.52895 12.3012 8.46209 12.1778 8.3648C12.0545 8.2675 11.9558 8.14251 11.8898 7.99996L9.99976 4.24996L8.10976 7.99996C8.03741 8.14366 7.93145 8.26779 7.80089 8.3618C7.67032 8.45581 7.51899 8.51692 7.35976 8.53996L3.15976 9.15996L6.15976 12.09C6.27704 12.2034 6.36478 12.3438 6.41533 12.4989C6.46588 12.6541 6.4777 12.8192 6.44976 12.98L5.72976 17.14L9.49976 15.14C9.65951 15.0806 9.83261 15.0667 9.99976 15.1Z"
-                      fill="#fefae8"
-                    />
-                  </svg>
+                  <img src="/star_bright.svg" alt="star" />
+                  <img src="/star_bright.svg" alt="star" />
+                  <img src="/star_bright.svg" alt="star" />
+                  <img src="/star_bright.svg" alt="star" />
+                  <img src="/star_dark.svg" alt="no star" />
                 </div>
                 <p className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 font-normal text-base leading-4 hover:underline hover:duration-100 cursor-pointer">{product.reviews.length} reviews</p>
               </div>
@@ -59,9 +33,11 @@ async function Product4 ({ params }) {
                     <label htmlFor="quantity" className="flex flex-row justify-between items-center w-full">
                       <p className="text-base leading-4 ">Select quantity ({product.stock} in stock)</p>
                       <select name="quantity" id="quantity" className="w-1/4 text-black px-2 text-md text-center">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
+                        {[...Array(product.stock)].map((item, i) => {
+                          return (
+                            <option key={i + 1} value={i + 1}>{i + 1}</option>
+                          )
+                        })}
                       </select>
                     </label>
                   </div>
@@ -76,15 +52,17 @@ async function Product4 ({ params }) {
                     <div className="text-white">Depth: {product.dimensions.depth}&quot;</div>
                     <div className="text-white">Height: {product.dimensions.height}&quot;</div>
                     <div className="text-white">Weight: {product.weight} oz.</div>
-                    <div className="text-white">Minimum Order: {product.minimumOrderQuanity}</div>
                     <div className="text-white capitalize">Category: {product.category}</div>
                     <div className="text-white capitalize">{product.warrantyInformation}</div>
                     <div className="text-white capitalize">{product.shippingInformation}</div>
                     <div className="text-white capitalize">Return Policy: {product.returnPolicy}</div>
-                    <div className="text-white capitalize">Tags:
+                    <div className="text-white">Tags:
                       {product.tags.map((tag, idx) => {
                         return (
-                          <div key={idx} className="text-white capitalize">{tag}</div>
+                          <>
+                            <span key={idx} className="text-white capitalize mx-2">{tag}</span>
+                            {idx !== product.tags.length - 1 ? <span>/</span> : null}
+                          </>
                         )
                       })}
                     </div>
@@ -95,7 +73,6 @@ async function Product4 ({ params }) {
               </div>
               <button className="focus:outline-none focus:ring-2 hover:bg-royal-blue-500 focus:ring-offset-2 focus:ring-gray-800 font-medium text-base leading-4 text-white bg-royal-blue-600 w-full py-5 lg:mt-12 mt-6">Add to shopping bag</button>
             </div>
-
             <div className=" w-full flex flex-row gap-4">
               <div className=" w-full grid grid-cols-1 gap-6">
               {product.images.map((image, idx) => {
@@ -110,7 +87,7 @@ async function Product4 ({ params }) {
           </div>
           <div className="mt-4">
             <h4>Reviews</h4>
-            <div className=" flex flex-col gap-4">
+            <div className=" flex flex-col gap-4 mt-2">
               {product.reviews.map((review, idx) => {
                 return (
                   <div key={idx} className="font-medium text-base leading-4 w-full">
@@ -119,7 +96,7 @@ async function Product4 ({ params }) {
                       <p className="text-white text-base product-details-inner">{review.comment}</p>
                     </div>
                     <div className="">
-                      <p className="text-white text-base product-details-inner text-right italic">{review.reviewerName}</p>
+                      <p className="text-white text-sm product-details-inner text-right italic">{review.reviewerName}</p>
                     </div>
                   </div>
                 )
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,10 +2,8 @@
 import Link from "next/link"
 
 export default async function ProductsList () {
-  const data = await fetch('https://dummyjson.com/products')
+  const data = await fetch('https://dummyjson.com/products?limit=40')
   const { products } = await data.json()
-
-  console.log('products', products)
 
   return (
     <div className="">
@@ -13,14 +11,14 @@ export default async function ProductsList () {
       <div className="font-dm-sans">
         <p className="px-3 text-base">Click a product for details.</p>
         <ul role="list" className="px-5 m-1 flex gap-6 py-5 overflow-auto whitespace-nowrap">
-          {products.length > 0 && products.map((product) => (
-            <li key={product.id} className="min-w-[18rem] h-[27rem] py-2 px-3 bg-t-med-brown rounded-2xl">
+          {products.length > 0 && products.map((product, idx) => (
+            <li key={idx} className="min-w-[18rem] h-[27rem] py-2 px-3 bg-t-med-brown rounded-2xl">
               <Link href={`/product/${product.id}`} className="">
                 <div className="flex flex-col gap-3 items-stretch">
                   <p className="text-left text-sm capitalize h-4">{product.category}</p>
                   <img alt="" src={product.thumbnail} className="h-52 w-52 flex-none bg-gray-800 mx-auto" />
                   <h3 className="text-base font-marion text-wrap text-left min-h-20">{product.title}</h3>
-                  <p className="text-t-light-blue text-left tracking-wide text-base">Rating: {product.rating} ({product.reviews.length} reviews)</p>
+                  <p className="text-t-light-blue text-left tracking-wide text-base">Rating: {product.rating.toFixed(1)} ({product.reviews.length} reviews)</p>
                   <p className="text-left">${product.price.toFixed(2)}</p>
                 </div>
               </Link>

7d499087a84fd0add4228670baf3e5566857c088
paunchymonk <gregory@dickison.net>
2024-11-05 05:57:24 -0800

    Add sign up and sign in to sidebar menu

--- src/components/SidebarNav.js
+++ src/components/SidebarNav.js
@@ -8,6 +8,8 @@ function SidebarNav() {
       <div className="sidenav__links">
         <Link href={'/'}>Home</Link>
         <Link href={'/design-system'}>Design System</Link>
+        <Link href={'/sign-up'}>Sign Up</Link>
+        <Link href={'/sign-in'}>Sign In</Link>
         <Link href={'/business-landing-screen'}>Business Landing Screen</Link>
         <Link href={'/profile-settings'}>Profile Settings Component</Link>
         <Link href={'/toggle-switch'}>Toggle Switch Component</Link>

ddf345f24f4231ad3838cc13c10b422b769beed5
paunchymonk <gregory@dickison.net>
2024-11-05 18:07:06 -0800

    Add Talents styles to product detail view

---
+++ src/components/CategoryPill.js
@@ -0,0 +1,7 @@
+const CategoryPill = ({category}) => {
+  return (
+    <p className="font-montserrat text-sm capitalize px-2 py-0.5 rounded-full bg-energy-yellow-300/50">{category}</p>
+  )
+}
+
+export default CategoryPill
